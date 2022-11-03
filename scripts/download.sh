ddaff98c39b13a2de579c558de26aaf4ab9f0490
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-11-03 14:01:33 -0400

    Add download script

---
+++ scripts/download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source scripts/utils.sh echo -n
+
+# Saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail
+
+# This script is meant to be used with the command 'datalad run'
+
+files_url=(
+	"http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar VOCtrainval_11-May-2012.tar")
+
+# These urls require login cookies to download the file
+git-annex addurl --fast -c annex.largefiles=anything --raw --batch --with-files <<EOF
+$(for file_url in "${files_url[@]}" ; do echo "${file_url}" ; done)
+EOF
+git-annex get --fast -J8
+git-annex migrate --fast -c annex.largefiles=anything *
+
+[[ -f md5sums ]] && md5sum -c md5sums
+[[ -f md5sums ]] || md5sum $(list -- --fast) > md5sums
